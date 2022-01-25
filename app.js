0f222180a6cc429d9a5244dc177f13f7ccaa3bc4
JangCo <90555595+JangCo@users.noreply.github.com>
2022-01-25 23:50:10 +0900

    day3
    Clone study

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 const time = document.querySelector(".status-bar__time");
+
+function setTime() {
+    const nowdate = new Date();
+    let hour = String(nowdate.getHours()).padStart(2, "0");
+    let min = String(nowdate.getMinutes()).padStart(2, "0");
+
+    time.innerHTML = hour + " : " + min;
+}
+
+setTime();
+setInterval(setTime, 1000);
